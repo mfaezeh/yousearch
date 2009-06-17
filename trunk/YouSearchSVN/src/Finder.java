a439285d0e043bc9720ed063f6a8d4aac7fe7ded
turricatch <turricatch@fc3f47e6-5993-11de-bc7c-c9df4fa8b3c6>
2009-06-17 07:49:55 +0000

    ho uppato il nuovo finder,che ha la funzione setProxy xke a me in uni non esce se non metto il proxy. E' cmq sufficiente commentare la chiamata per evitare che lo richiama. Dori
    PS:Sul portatile mi va l'xml,son quasi sicuro che è un problema di librerie,pomeriggio a casa vedo di far andare tutto anche sul fisso

--- trunk/YouSearchSVN/src/Finder.java
+++ trunk/YouSearchSVN/src/Finder.java
@@ -2,9 +2,20 @@ import it.unitn.datamining.yousearch.YouSearch;
 
 
 public class Finder {
+	private static final String PROXY_URL  = "proxy.science.unitn.it";
+	private static final String PROXY_PORT = "3128";
+	
 	public static void main(String[] args){
-		
+		//setProxy();
 		YouSearch finder = new YouSearch("movie");
 	}
 
+	public static void setProxy(){
+		
+		System.getProperties().put( "proxySet", "true" );
+		System.getProperties().put( "proxyHost", PROXY_URL);
+		System.getProperties().put( "proxyPort", PROXY_PORT);
+		
+		
+	}
 }
